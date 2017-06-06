e0f1554d4a70269348fe1871c0c70205aab43e43
Alex <alsam@uber.com>
2017-06-06 10:39:16 -0700

    Make WithDefault to provide a deep copy of the provided default value (#17)
    WithDefaultValue replaces only a default value, which makes it to work only with scalar types, but more complex types will fail.
    We can update the provider saved in value to be a grouped provider of the original provider and a static provider based on the provided value.

--- static_provider_test.go
+++ static_provider_test.go
@@ -295,3 +295,41 @@ func TestValue_ChildKeys(t *testing.T) {
 	p = NewStaticProvider(map[int]string{3: "three", 5: "five"})
 	t.Run("MapOfInts", func(t *testing.T) { op(t, Root, []string{"3", "5"}) })
 }
+
+func TestConfigDefaults(t *testing.T) {
+	t.Parallel()
+
+	type cfg struct{ N int }
+
+	var c cfg
+	require.NoError(
+		t,
+		NewStaticProvider(nil).Get("metrics").WithDefault(cfg{42}).Populate(&c),
+		"Failed to populate config.",
+	)
+	assert.Equal(t, cfg{N: 42}, c)
+}
+
+func TestConfigDefaultsAreOverriddenByHigherPriorityProviders(t *testing.T) {
+	t.Parallel()
+
+	type book struct {
+		Title  string
+		Author string
+		Year   int
+	}
+
+	var novel book
+	require.NoError(
+		t,
+		NewStaticProvider(map[string]string{
+			"library.author": "Dreiser",
+			"library.title":  "The Financier"},
+		).Get("library").WithDefault(book{
+			Title: "An American Tragedy",
+			Year:  1925,
+		}).Populate(&novel),
+		"Failed to write a novel.",
+	)
+	assert.Equal(t, book{Title: "The Financier", Author: "Dreiser", Year: 1925}, novel)
+}
