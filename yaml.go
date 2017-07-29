fad374580d89210e6a96709e20b6b95bba37eff2
Alexandr Samylkin <alsam@uber.com>
2017-07-29 11:45:34 -0700

    Comments typos

--- yaml.go
+++ yaml.go
@@ -323,7 +323,7 @@ func (n *yamlNode) Children() []*yamlNode {
 }
 
 // Apply expand to all nested elements of a node.
-// There is no need to use reflectionYAML unmarshaler is using
+// There is no need to use reflection, because YAML unmarshaler is using
 // map[interface{}]interface{} to store objects and []interface{}
 // to store collections.
 func recursiveApply(node interface{}, expand func(string) string) interface{} {
